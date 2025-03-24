aae46ee7fd05dd1c56a5f240ba36176e05dfc0ae
RL01521 <rl0151032024@unab.edu.sv>
2025-03-24 16:46:18 -0600

    EL y clase Telefono - Javier Lopez

---
+++ Sistema Agenda de Contactos/EL/Telefono.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EL
+{
+    public class Telefono
+    {
+
+        private static int contadorId = 1;
+        private int id;
+        private string numero;
+        private string tipo; // "Casa", "Trabajo", etc.
+
+
+        public int Id => id; // Solo lectura
+        public string Numero
+        {
+            get => numero;
+            set => numero = !string.IsNullOrWhiteSpace(value) ? value : throw new ArgumentException("El número no puede estar vacío.");
+        }
+        public string Tipo
+        {
+            get => tipo;
+            set => tipo = !string.IsNullOrWhiteSpace(value) ? value : throw new ArgumentException("El tipo no puede estar vacío.");
+        }
+
+
+        public int ContactoId { get; set; }
+
+        public Telefono(string numero, string tipo, int contactoId)
+        {
+            id = contadorId++;
+            Numero = numero;
+            Tipo = tipo;
+            ContactoId = contactoId;
+
+
+        }
+    }
+}
+
