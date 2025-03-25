d945b08022bc6adee71ded99290e2414767b74ee
RL01521 <rl0151032024@unab.edu.sv>
2025-03-24 18:48:51 -0600

    Cambios de clase - Javier Lopez

---
+++ Sistema Agenda de Contactos/EL/Contacto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EL
+{
+    public abstract class Contacto
+    {
+        private string _nombre;
+        private string _telefono;
+        private string _correo;
+
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        public string Telefono { get => _telefono; set => _telefono = value; }
+        public string Correo { get => _correo; set => _correo = value; }
+
+        public Contacto(string nombre, string telefono, string correo)
+        {
+            _nombre = nombre;
+            _telefono = telefono;
+            _correo = correo;
+        }
+
+        public abstract void MostrarInformacion();
+    }
+}
+
