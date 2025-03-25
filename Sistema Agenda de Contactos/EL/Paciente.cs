57b3dabb4400e1daee98d9a18a4ce9bd45e325e4
Cherryhei <vf0352032024@unab.edu.sv>
2025-03-25 13:57:09 -0600

    Clase Paciente- Heidy Ventura

---
+++ Sistema Agenda de Contactos/EL/Paciente.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EL
+{
+        public class Paciente : Contacto
+        {
+
+            public int Id { get; private set; }
+            public string Apellido { get; private set; }
+            public DateTime FechaNacimiento { get; private set; }
+
+
+            public Paciente(int id, string nombre, string apellido, string telefono, string correo, DateTime fechaNacimiento)
+                : base(nombre, telefono, correo)
+            {
+                Id = id;
+                Apellido = apellido;
+                FechaNacimiento = fechaNacimiento;
+            }
+
+            public int Edad()
+            {
+                int edad = DateTime.Now.Year - FechaNacimiento.Year;
+                if (DateTime.Now.DayOfYear < FechaNacimiento.DayOfYear)
+                    edad--;
+                return edad;
+            }
+
+
+            public override void MostrarInformacion()
+            {
+                Console.WriteLine($"{Nombre} {Apellido}, Tel: {Telefono}, Edad: {Edad()}");
+            }
+
+
+            public override string ToString()
+            {
+                return $"{Nombre} {Apellido}, Tel: {Telefono}, Edad: {Edad()}";
+            }
+        }
+
+
+    }
+
